24630c2e8ef4d5f0e43c8ebc0e6e25e16f807bff
Samuel Pietra <samuelpietra@outlook.com>
2024-02-26 18:09:37 -0300

    feat: creates Layout.Screen compound component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,10 @@
-import { PropsWithChildren } from 'react'
+import { CSSProperties, PropsWithChildren } from 'react'
 
+import { Column } from '../Column'
 import * as S from './Layout.styles'
 import { TopBar } from './TopBar'
+
+const MIN_WIDTH = 1110
 
 interface LayoutProps {
   children: React.ReactNode
@@ -19,11 +22,21 @@ function Layout({ children }: PropsWithChildren<LayoutProps>) {
   )
 }
 
+const Screen = ({ children, ...props }: PropsWithChildren<CSSProperties>) => {
+  return (
+    <Column width="100%" {...props}>
+      {children}
+    </Column>
+  )
+}
+
 const Sheet = ({ children, ...props }: PropsWithChildren<S.SheetProps>) => {
   return <S.Sheet {...props}>{children}</S.Sheet>
 }
 
 Layout.Content = S.Content
+Layout.Screen = Screen
 Layout.Sheet = Sheet
+Layout.MIN_WIDTH = MIN_WIDTH
 
 export { Layout }
