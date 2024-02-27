972bb699755363f8bf3a5242701ecdde53f3dffa
Samuel Pietra <samuelpietra@outlook.com>
2024-02-27 18:54:33 -0300

    refactor: swaps Icon for IconButton on TopBar

--- src/components/Layout/TopBar/TopBar.tsx
+++ src/components/Layout/TopBar/TopBar.tsx
@@ -1,5 +1,7 @@
 import { FaGithub } from 'react-icons/fa'
 
+import { IconButton } from '@mui/material'
+
 import fullLogo from '@/assets/full-logo.svg'
 
 import * as S from './TopBar.styles'
@@ -9,9 +11,12 @@ function TopBar() {
     <S.TopBar>
       <img src={fullLogo} alt="logo" height={45} />
 
-      <a href="https://github.com/samuelpietra/hostfully" target="_blank">
-        <FaGithub aria-label="FaGithub" size={32} />
-      </a>
+      <IconButton
+        style={{ padding: 0 }}
+        onClick={() => window.open('https://github.com/samuelpietra/hostfully', '_blank')}
+      >
+        <FaGithub size={32} />
+      </IconButton>
     </S.TopBar>
   )
 }
