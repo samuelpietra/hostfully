b2473af203d0d73e49d927a49c3cd41c55305d71
Samuel Pietra <31863795+samuelpietra@users.noreply.github.com>
2024-02-27 13:30:11 -0300

    style: turns BookingDetails page responsive (#6)

--- src/pages/BookingDetails/components/Result/Result.tsx
+++ src/pages/BookingDetails/components/Result/Result.tsx
@@ -1,16 +1,16 @@
-import { IoPersonSharp } from 'react-icons/io5'
+import { useMemo } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-import { TextField } from '@mui/material'
-
 import { GetBookingDetailsAPI } from '@/api-models/bookings'
-import { Column } from '@/components/Column'
+import { Layout } from '@/components/Layout'
 import { LoadingContainer } from '@/components/LoadingContainer'
-import { Row } from '@/components/Row'
 import { StateMessage } from '@/components/StateMessage'
 import { HTTPStatusCode } from '@/constants/HTTPStatusCode'
-import dateFormat from '@/infrastructure/dateFormat'
+import useWindowDimensions from '@/hooks/useWindowDimensions'
 import { ApiErrorResponse } from '@/services/http/errorInterceptor'
+
+import { MobileForm } from '../MobileForm'
+import { WebForm } from '../WebForm'
 
 interface ResultProps {
   data?: GetBookingDetailsAPI.GetResponse
@@ -21,6 +21,9 @@ interface ResultProps {
 
 function Result({ loading, onRetry, data, error }: ResultProps) {
   const navigate = useNavigate()
+  const dimensions = useWindowDimensions()
+
+  const Component = useMemo(() => (dimensions.width < Layout.MIN_WIDTH ? MobileForm : WebForm), [dimensions])
 
   if (loading) return <LoadingContainer title="Loading details..." />
 
@@ -40,87 +43,7 @@ function Result({ loading, onRetry, data, error }: ResultProps) {
 
   if (!data) return null
 
-  return (
-    <Column>
-      <Column marginBottom={72}>
-        <Row alignItems="center" borderBottom="1px dashed #555" marginBottom={24} paddingBottom={8}>
-          <IoPersonSharp color="#3dc299" size={22} />
-          <h2 style={{ marginLeft: 16 }}>Customer</h2>
-        </Row>
-
-        <Row>
-          <Column marginRight={100} width={220}>
-            <TextField
-              label="Name"
-              defaultValue={data.customer.name}
-              fullWidth
-              InputProps={{
-                readOnly: true
-              }}
-              variant="standard"
-            />
-          </Column>
-
-          <Column marginRight={100} width={120}>
-            <TextField
-              label="Document"
-              defaultValue={data.customer.document}
-              fullWidth
-              InputProps={{
-                readOnly: true
-              }}
-              variant="standard"
-            />
-          </Column>
-
-          <Column width={120}>
-            <TextField
-              label="Phone"
-              defaultValue={data.customer.document}
-              fullWidth
-              InputProps={{
-                readOnly: true
-              }}
-              variant="standard"
-            />
-          </Column>
-        </Row>
-      </Column>
-
-      <Column>
-        <Row alignItems="center" borderBottom="1px dashed #555" marginBottom={24} paddingBottom={8}>
-          <IoPersonSharp color="#3dc299" size={22} />
-          <h2 style={{ marginLeft: 16 }}>Period</h2>
-        </Row>
-
-        <Row>
-          <Column marginRight={140} width={180}>
-            <TextField
-              label="Check-in"
-              defaultValue={dateFormat.format(data.checkInAt)}
-              fullWidth
-              InputProps={{
-                readOnly: true
-              }}
-              variant="standard"
-            />
-          </Column>
-
-          <Column marginRight={100} width={180}>
-            <TextField
-              label="Check-out"
-              defaultValue={dateFormat.format(data.checkOutAt)}
-              fullWidth
-              InputProps={{
-                readOnly: true
-              }}
-              variant="standard"
-            />
-          </Column>
-        </Row>
-      </Column>
-    </Column>
-  )
+  return <Component {...data} />
 }
 
 export { Result }
