0fa29003556a49e707f61231746c24615600d132
Samuel Pietra <samuelpietra@outlook.com>
2024-02-27 18:47:00 -0300

    feat: applies cache logic to MyBookings page

--- src/pages/MyBookings/MyBookings.tsx
+++ src/pages/MyBookings/MyBookings.tsx
@@ -1,36 +1,68 @@
 import { useEffect, useMemo } from 'react'
+import { IoMdRefresh } from 'react-icons/io'
+import { useSearchParams } from 'react-router-dom'
+
+import { IconButton } from '@mui/material'
 
 import { ListBookingsAPI } from '@/api-models/bookings'
 import { Layout } from '@/components/Layout'
+import { Row } from '@/components/Row'
 import useHttpStateful from '@/hooks/useHttpStateful'
 import usePageTitle from '@/hooks/usePageTitle'
 import useWindowDimensions from '@/hooks/useWindowDimensions'
+import { useBookingsStore } from '@/store/bookings'
 
 import { Result } from './components/Result'
 
 function MyBookingsPage() {
   usePageTitle('My Bookings')
 
+  const { storedList, setStoredList } = useBookingsStore()
+  const [searchParams, setSearchParams] = useSearchParams()
+
   const dimensions = useWindowDimensions()
 
+  const shouldRefresh = useMemo(() => searchParams.get('refresh') === 'true', [searchParams])
   const Wrapper = useMemo(() => (dimensions.width < Layout.MIN_WIDTH ? Layout.Screen : Layout.Sheet), [dimensions])
 
   const {
     isLoading: isLoadingBookings,
-    payload: bookingsList,
     request: listBookings,
     error: listBookingsError
   } = useHttpStateful<ListBookingsAPI.GetResponse, ListBookingsAPI.RequestParams>('get', '/bookings')
 
   useEffect(() => {
-    void listBookings()
-  }, [])
+    if (shouldRefresh || !storedList) {
+      const fetchNewData = async () => {
+        const { payload } = await listBookings()
+
+        if (payload) {
+          setStoredList(payload)
+          searchParams.delete('refresh')
+          setSearchParams(searchParams)
+        }
+      }
+
+      void fetchNewData()
+    }
+  }, [listBookings, searchParams, setSearchParams, setStoredList, shouldRefresh, storedList])
 
   return (
     <Wrapper>
-      <h1 style={{ marginBottom: 24 }}>My bookings</h1>
+      <Row alignItems="center" marginBottom={24}>
+        <h1 style={{ marginRight: 8 }}>My bookings</h1>
 
-      <Result data={bookingsList} error={listBookingsError} loading={isLoadingBookings} onRetry={listBookings} />
+        <IconButton onClick={() => void listBookings()}>
+          <IoMdRefresh size={32} />
+        </IconButton>
+      </Row>
+
+      <Result
+        data={storedList}
+        error={listBookingsError}
+        loading={isLoadingBookings}
+        onRetry={() => void listBookings()}
+      />
     </Wrapper>
   )
 }
